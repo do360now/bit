ff299ceffb27806a62a92b49cbcbcb25e77bdca9
Clement <cm@do360now.com>
2024-11-29 20:37:29 +0100

    remove yield

--- Bit/config.py
+++ Bit/config.py
@@ -18,12 +18,12 @@ API_DOMAIN = os.getenv("API_DOMAIN", "https://api.kraken.com")
 # Allocation strategy for portfolio management
 ALLOCATIONS = {
     'HODL': float(os.getenv("ALLOC_HODL", 0.7)),
-    'YIELD': float(os.getenv("ALLOC_YIELD", 0.15)),
-    'TRADING': float(os.getenv("ALLOC_TRADING", 0.15))
+    'YIELD': float(os.getenv("ALLOC_YIELD", 0.00)),
+    'TRADING': float(os.getenv("ALLOC_TRADING", 0.3)),
 }
 
 # Initial BTC balance
-TOTAL_BTC = float(os.getenv("TOTAL_BTC", 0.00759185))
+TOTAL_BTC = float(os.getenv("TOTAL_BTC", 0.0058933102))
 
 # Minimum trading volume to avoid very small trades
 MIN_TRADE_VOLUME = float(os.getenv("MIN_TRADE_VOLUME", 0.0001))
